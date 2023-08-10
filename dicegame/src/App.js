eaee474e9c334e78f74489856d9741f50567faec
kimInDa <pinktail2327@naver.com>
2023-08-10 22:33:35 +0900

    [Add] childern 학습

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -1,8 +1,15 @@
+import Button from "./Button"
 import Dice from "./Dice"
 
 function App() {
   return (
-    <Dice color="red" num={2} />
+    <div>
+      <div>
+        <Button>던지기</Button>
+        <Button>처음부터</Button>
+      </div>
+      <Dice color="red" num={2} />
+    </div>
   )
 }
 
