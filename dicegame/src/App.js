d5f82fc5b8311c4ac259bfd57e4b71e00408f7d1
kimInDa <pinktail2327@naver.com>
2023-08-15 22:48:05 +0900

    [Add] 주사위 보드 부분 컴포넌트화

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Button from "./Button"
-import Dice from "./Dice"
+import Board from './Board';
+
 
 function random(n) {
   return Math.ceil(Math.random() * n);
@@ -10,18 +11,30 @@ function App() {
   const [num, setNum] = useState(1);
   const [sum, setSum] = useState(0);
   const [gameHistory, setGameHistory] = useState([]);
+  const [otherNum, setOtherNum] = useState(1);
+  const [otherSum, setOtherSum] = useState(0);
+  const [otherGameHistory, setOtherGameHistory] = useState([]);
+
+
 
   const handleRollClick = () => {
     const nextNum = random(6);
+    const otherNextNum = random(6);
     setNum(nextNum);
     setSum(sum + nextNum);
     setGameHistory([...gameHistory, nextNum]);
+    setOtherNum(otherNextNum);
+    setOtherSum(otherSum + otherNextNum);
+    setOtherGameHistory([...otherGameHistory, otherNextNum]);
   }
 
   const handleClearClick = () => {
     setNum(1);
     setSum(0)
     setGameHistory([])
+    setOtherNum(1);
+    setOtherSum(0)
+    setOtherGameHistory([])
   }
 
   return (
@@ -30,13 +43,11 @@ function App() {
         <Button onClick={handleRollClick}>던지기</Button>
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      <Dice color="blue" num={num} />
-      <h2>총점</h2>
-      <p>{sum}</p>
-      <h2>기록</h2>
-      <p>{gameHistory.join(', ')}</p>
+      <div>
+        <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory}/>
+        <Board name="상대" color="red" num={otherNum} sum={otherSum} gameHistory={otherGameHistory}/>
+      </div>
     </div>
-    
   )
 }
 
