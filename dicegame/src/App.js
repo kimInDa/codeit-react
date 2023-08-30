2bad335c195ac63cfacc0c553939e28fc1ebffe7
kimInDa <pinktail2327@naver.com>
2023-08-30 22:09:15 +0900

    스타일 적용 및 완성

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Button from './Button';
 import Board from './Board';
 import './App.css';
+import logoImage from './assets/logo.png';
 
 function random(n) {
   return Math.ceil(Math.random() * n);
@@ -26,6 +27,10 @@ function App() {
   return (
     <div className='App'>
       <div>
+        <img src={logoImage} alt='주사위' className='App-logo' />
+        <h1 className='App-title'>주사위 게임</h1>
+      </div>
+      <div>
         <Button className='App-button' color='blue' onClick={handleRollClick}>
           던지기
         </Button>
@@ -33,9 +38,19 @@ function App() {
           처음부터
         </Button>
       </div>
-      <div>
-        <Board name='나' color='blue' gameHistory={myHistory} />
-        <Board name='상대' color='red' gameHistory={otherHistory} />
+      <div className='App-boards'>
+        <Board
+          className='App-board'
+          name='나'
+          color='blue'
+          gameHistory={myHistory}
+        />
+        <Board
+          className='App-board'
+          name='상대'
+          color='red'
+          gameHistory={otherHistory}
+        />
       </div>
     </div>
   );
