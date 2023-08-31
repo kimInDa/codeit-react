49a1d4db0c5c8f7f9ecc6517ea11cc3350a74d8f
kimInDa <pinktail2327@naver.com>
2023-08-31 17:32:53 +0900

    [푸드잇] sort 정렬 기능 추가

--- foodit/src/App.jsx
+++ foodit/src/App.jsx
@@ -1,11 +1,21 @@
 import FoodList from './components/FoodList';
 import items from './mock.json';
+import { useState } from 'react';
 
 function App() {
+  const [order, setOrder] = useState('createdAt');
+
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder('createdAt');
+  const handleCalorieClick = () => setOrder('calorie');
+
   return (
-    <>
-      <FoodList items={items} />
-    </>
+    <div>
+      <button onClick={handleNewestClick}>최신순</button>
+      <button onClick={handleCalorieClick}>칼로리순</button>
+      <FoodList items={sortedItems} />
+    </div>
   );
 }
 
