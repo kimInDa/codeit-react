61758d00dd9feb98294d98980ba491b26d9c25de
kimInDa <pinktail2327@naver.com>
2023-08-31 22:19:27 +0900

    [무비피디아] fetch 사용하기

--- moviepedia/src/App.jsx
+++ moviepedia/src/App.jsx
@@ -1,10 +1,11 @@
-import ReviewList from './components/ReviewList';
-import mockItems from './mock.json';
 import { useState } from 'react';
+import ReviewList from './components/ReviewList';
+// import mockItems from './mock.json';
+import { getReviews } from './api';
 
 function App() {
   const [order, setOrder] = useState('createdAt');
-  const [items, setItems] = useState(mockItems);
+  const [items, setItems] = useState([]);
 
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
@@ -16,6 +17,11 @@ function App() {
     setItems(nextItems);
   };
 
+  const handleLoadClick = async () => {
+    const { reviews } = await getReviews();
+    setItems(reviews);
+  };
+
   return (
     <div>
       <div>
@@ -23,6 +29,7 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
