d1fa2ea73c10044f75a0a814e245a2123bc7fd54
kimInDa <pinktail2327@naver.com>
2023-09-02 00:25:37 +0900

    [무비피디아] ref로 DOM 노드 가져오기

--- moviepedia/src/components/FileInput.jsx
+++ moviepedia/src/components/FileInput.jsx
@@ -1,10 +1,20 @@
+import { useRef, useEffect } from 'react';
+
 function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
-  return <input type='file' onChange={handleChange} />;
+  useEffect(() => {
+    if (inputRef.current) {
+      console.log(inputRef.current);
+    }
+  }, []);
+
+  return <input type='file' onChange={handleChange} ref={inputRef} />;
 }
 
 export default FileInput;
