5d814a53bb1831c5829d31a945e3864e895e4371
kimInDa <pinktail2327@naver.com>
2023-09-02 00:13:35 +0900

    [무비피디아] File Input 기능 구현

--- moviepedia/src/components/ReviewForm.jsx
+++ moviepedia/src/components/ReviewForm.jsx
@@ -1,20 +1,25 @@
 import { useState } from 'react';
 import './ReviewForm.css';
+import FileInput from './FileInput';
 
 function ReviewForm() {
   const [values, setValues] = useState({
     title: '',
     rating: 0,
     content: '',
+    imgFile: null,
   });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
+  const handleChange = (name, value) => {
     setValues((preValues) => ({
       ...preValues,
       [name]: value,
     }));
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
   };
 
   const handleSubmit = (e) => {
@@ -24,19 +29,28 @@ function ReviewForm() {
 
   return (
     <form onSubmit={handleSubmit} className='ReviewForm'>
+      <FileInput
+        name='imgFile'
+        value={values.imgFile}
+        onChange={handleChange}
+      />
       <input
         name='title'
         type='text'
         value={values.title}
-        onChange={handleChange}
+        onChange={handleInputChange}
       />
       <input
         name='rating'
         type='number'
         value={values.rating}
-        onChange={handleChange}
+        onChange={handleInputChange}
       />
-      <textarea name='content' value={values.content} onChange={handleChange} />
+      <textarea
+        name='content'
+        value={values.content}
+        onChange={handleInputChange}
+      />
       <button type='submit'>확인</button>
     </form>
   );
