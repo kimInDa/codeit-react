6ea558f09357f15ae93d28eb56b56fecce5e4c82
kimInDa <pinktail2327@naver.com>
2023-09-01 16:38:34 +0900

    [무비피디아] 조건부 렌더링을 이용하여 데이터 버튼이 있을 때만 더보기 버튼을 보여주기

--- moviepedia/src/App.jsx
+++ moviepedia/src/App.jsx
@@ -47,9 +47,7 @@ function App() {
         <button onClick={handleBestClick}>베스트순</button>
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
-      <button onClick={handleLoadMore} disabled={!hasNext}>
-        더 보기
-      </button>
+      {hasNext && <button onClick={handleLoadMore}>더 보기</button>}
     </div>
   );
 }
